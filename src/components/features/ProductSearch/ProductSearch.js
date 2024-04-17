2b77f41d6cf49e1b891a724dc0c05ad510d98e2a
DawSeg <buler43221@gmail.com>
2024-04-17 23:57:34 +0200

    addCategoriesToMenu

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
 import styles from './ProductSearch.module.scss';
+import { getAll } from '../../../redux/categoriesRedux';
+import { useSelector } from 'react-redux';
 
-const ProductSearch = () => (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={'d-none d-sm-block ' + styles.icon} icon={faListUl} />
-      <select className='ps-1 ' name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-    </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-);
+const ProductSearch = () => {
+  const categories = useSelector(getAll);
+
+  return (
+    <form action='' className={styles.root}>
+      <div className={styles.category}>
+        <FontAwesomeIcon className={'d-none d-sm-block ' + styles.icon} icon={faListUl} />
+        <ul className={styles.list}>
+          <li className={styles.visibleList}>
+            <span className={styles.hide}>Select a category</span>
+            {categories.map(category => (
+              <ul className={styles.hiddenList} key={category.id}>
+                <li className={styles.categoryButton}>{category.name}</li>
+              </ul>
+            ))}
+          </li>
+          <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+        </ul>
+      </div>
+      <div className={styles.searchField}>
+        <input placeholder='Search products...' type='text' />
+        <button>
+          <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        </button>
+      </div>
+    </form>
+  );
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
