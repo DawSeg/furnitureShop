070e97d0f0737f82b58e6f629c184b3b08eaa0f4
DawSeg <buler43221@gmail.com>
2024-05-12 16:37:58 +0200

    addSalesComponent

--- src/components/features/Sales/Sales.js
+++ src/components/features/Sales/Sales.js
@@ -10,9 +10,9 @@ const Sales = () => {
 
   return (
     <Container>
-      <div className={styles.Sales}>
+      <div className={styles.sales}>
         <div className={styles.leftSide}>
-          <img src={products[50].image}></img>
+          <img src={products[50].image} alt={products[50].image}></img>
           <div className={styles.saleDescription}>
             <h3>Guest room <span><br />{products[50].category}</span> </h3>
             <p>-20%</p>
@@ -20,19 +20,23 @@ const Sales = () => {
         </div>
         <div className={styles.rightSide}>
           <div className={styles.topSection}>
-            <img className={styles.productLeft} src={products[28].image}></img>
-            <img className={styles.productRight} src={products[29].image}></img>
+            <img className={styles.productLeft} src={products[28].image}
+              alt={products[28].image}>
+            </img>
             <div className={styles.topDescription}>
               <h3>
                 <span className={styles.bold}>office </span>
                 chair
                 <span className={styles.letterSpacing}> collection</span>
-                <p>$200</p>
+                <p>$ {products[28].price}</p>
               </h3>
             </div>
+            <img className={styles.productRight} src={products[29].image}
+              alt={products[29].image}>
+            </img>
           </div>
           <div className={styles.bottomSection}>
-            <img src={products[13].image}></img>
+            <img src={products[13].image} alt={products[13].image}></img>
             <div className={styles.bottomDescription}>
               <h3><span>special</span> collection</h3>
               <p>save up to 45% of furniture</p>
