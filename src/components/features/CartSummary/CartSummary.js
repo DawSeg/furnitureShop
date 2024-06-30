2b5f1dc417015bc27c06efad4d21675d6cb0d302
DawSeg <buler43221@gmail.com>
2024-06-30 17:32:03 +0200

    addBasicStructureCart&&CartSummary

---
+++ src/components/features/CartSummary/CartSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CartSummary.module.scss';
+
+const CartSummary = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default CartSummary;
---
+++ src/components/views/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Cart.module.scss';
+import CartSummary from '../../features/CartSummary/CartSummary';
+
+const Cart = () => {
+
+  return (
+    <CartSummary />
+  );
+};
+
+export default Cart;
